924788f5b23eb56fdc99fcb7d403ee6efc1c42e5
Ajay <aajay693@gmail.com>
2021-04-29 12:43:43 +0530

    rr

--- src/App.js
+++ src/App.js
@@ -9,7 +9,11 @@ import './App.css';
 function App() {
   
   return (
-    <div>
+    <div class="App">
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous"/>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js" integrity="sha384-SR1sx49pcuLnqZUnnPwx6FCym0wLsk5JZuNx2bPPENzswTNFaQU1RDvt3wT4gWFG" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.min.js" integrity="sha384-j0CNLUeiqtyaRmlzUHCPZ+Gy5fQu0dQ6eZ/xAww941Ai1SxSY+0EQqNXNE6DZiVc" crossorigin="anonymous"></script>
+
       <div class="jumbotron my-auto">
       
   
@@ -21,6 +25,7 @@ function App() {
         <Router history={history}>
           <Switch>
             <Route path="/" exact>
+              <br/>
               <div class="display-4  mb-1"><h1 class="homehead">CREDIT</h1></div>
               <br></br><br></br><br></br>
               
@@ -46,7 +51,7 @@ function App() {
         </Router>
         </center>
       </div>
-      
+      <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
     </div>
   );
 }
